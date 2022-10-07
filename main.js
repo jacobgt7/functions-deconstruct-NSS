6913e4c6d71d0be16f0bf1d7393eb7684a2ff847
Jacob Thomas <jacgretho@gmail.com>
2022-10-07 09:40:07 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const logInToComputer = () => {
+    console.log("Log in to the computer")
+}
+
+const logInToSteam = () => {
+    console.log("Log in to Steam account.")
+}
+
+const putOnHeadset = () => {
+    console.log("Put your headset on.")
+}
+
+const plugHeadsetIn = () => {
+    console.log("Plug the headset in to the computer.")
+}
+
+const enableLink = () => {
+    console.log("Follow prompts to enable quest link.")
+}
+
+const openSteamVR = () => {
+    console.log("Open steam vr from recent apps or from Desktop.")
+}
+
+const pressStart = () => {
+    console.log("Press start button on controller to open menu.")
+}
+
+const selectGame = () => {
+    console.log("Use trigger button to select the game you wish to play.")
+}
+
+logInToComputer()
+logInToSteam()
+putOnHeadset()
+plugHeadsetIn()
+enableLink()
+openSteamVR()
+pressStart()
+selectGame()
